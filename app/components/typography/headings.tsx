e21ba549db52b983aa22aa18fb9741e9d9d38f40
Ryan Ozawa <ryan.ozawa21@gmail.com>
2025-01-21 00:33:44 -0800

    [ui] add: pickover typography components
    source is from 'add/header'

---
+++ app/components/typography/headings.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/cn";
+
+// type imports
+import type { HeadingProps } from "./types";
+
+export function TypoH1({ className, children }: HeadingProps) {
+    return (
+        <h1
+            className={cn(
+                "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+                className,
+            )}
+        >
+            {children}
+        </h1>
+    );
+}
+
+export function TypoH2({ className, children }: HeadingProps) {
+    return (
+        <h2
+            className={cn(
+                "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+                className,
+            )}
+        >
+            {children}
+        </h2>
+    );
+}
+
+export function TypoH3({ className, children }: HeadingProps) {
+    return (
+        <h3
+            className={cn(
+                "scroll-m-20 text-2xl font-semibold tracking-tight",
+                className,
+            )}
+        >
+            {children}
+        </h3>
+    );
+}
+
+export function TypoH4({ className, children }: HeadingProps) {
+    return (
+        <h3
+            className={cn(
+                "scroll-m-20 text-xl font-semibold tracking-tight",
+                className,
+            )}
+        >
+            {children}
+        </h3>
+    );
+}
