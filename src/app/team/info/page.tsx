d91ea251e3a384c5a23c5549adc62e49b1480923
Ryan Ozawa <ryan.ozawa21@gmail.com>
2025-01-25 01:37:06 -0800

    move to client fetching

--- src/app/team/info/page.tsx
+++ src/app/team/info/page.tsx
@@ -1,35 +1,16 @@
 import { TypoH1 } from "@/components/typography/headings";
-import { Button } from "@/components/ui/button";
-import { queryAllTeamsQueryOptions } from "@/features/team-viewer/api/list-teams";
-import TeamCard from "@/features/team-viewer/components/team-card";
-import { getQueryClient } from "@/lib/query-client";
-import Link from "next/link";
+import TeamGrid from "./team-grid";
 
 // TODO<ryan>: handle renavigation to this page as an error redirect from team info page
 // if search param `error` is present, display the error message and then clear the search param
 
-export const dynamic = "force-dynamic";
-
-export default async function RouteComponent() {
-    const queryClient = getQueryClient();
-    const teams = await queryClient.ensureQueryData(
-        queryAllTeamsQueryOptions(),
-    );
-
+export default function RouteComponent() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-8">
             <TypoH1 className="text-4xl font-bold mb-8">
                 SLOLCS 2025 Teams
             </TypoH1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-                {teams.map((team) => (
-                    <TeamCard key={team.id} team={team}>
-                        <Button asChild>
-                            <Link href={`/team/${team.id}`}>View Team</Link>
-                        </Button>
-                    </TeamCard>
-                ))}
-            </div>
+            <TeamGrid />
         </main>
     );
 }
