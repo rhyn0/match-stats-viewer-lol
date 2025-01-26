20fd7b5a1943339fba66eed3b6b9bc12b3f33c0c
TeachMeTW <rsimps03@calpoly.edu>
2025-01-25 23:01:03 -0800

    nav bar update; made the link.description a popup and centered the nav
    mod
    
    updated nav to be centered, updated links description to be popup

--- src/components/navigation/bar.tsx
+++ src/components/navigation/bar.tsx
@@ -38,21 +38,31 @@ function MainNavigationBar() {
                             {navigation.triggerTitle}
                         </NavigationMenuTrigger>
                         <NavigationMenuContent>
-                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                                 {navigation.links.map((link) => (
-                                    <li key={link.label}>
+                                    <li key={link.label} className="relative group">
                                         <NavigationLink
                                             href={link.to}
                                             prefetch={false}
+                                            className="block p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors duration-200 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800"
+                                            aria-describedby={`desc-${key}-${link.label}`}
                                         >
-                                            {/* Due to implementation in NavigationLink, require singular child here */}
-                                            <>
-                                                <TypoH4>{link.label}</TypoH4>
-                                                <TypoP>
-                                                    {link.description}
-                                                </TypoP>
-                                            </>
+                                            <TypoH4 className="text-base font-medium text-gray-900 dark:text-white">
+                                                {link.label}
+                                            </TypoH4>
                                         </NavigationLink>
+                                        {/* Popup Description */}
+                                        <div
+                                            id={`desc-${key}-${link.label}`}
+                                            className="mt-1 w-64 p-2 bg-white dark:bg-gray-800 rounded-md shadow-lg opacity-0 max-h-0 overflow-hidden group-hover:opacity-100 group-hover:max-h-full transition-all duration-300 z-10"
+                                            role="tooltip"
+                                            aria-hidden="false"
+                                        >
+                                            <TypoP className="text-sm text-gray-700 dark:text-gray-300">
+                                                {link.description}
+                                            </TypoP>
+                                            <div className="absolute top-0 left-4 w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-b-4 border-b-white dark:border-b-gray-800"></div>
+                                        </div>
                                     </li>
                                 ))}
                             </ul>
