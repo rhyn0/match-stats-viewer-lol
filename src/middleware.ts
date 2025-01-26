c15ede1af7984c0261c1c9544344c471c8c894b5
Ryan Ozawa <ryan.ozawa21@gmail.com>
2025-01-25 21:03:04 -0800

    fix: middleware re-instate

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,6 @@ export { default } from "next-auth/middleware";
 
 export const config = {
     matcher: [
-        // "/upload", // deny access to upload page
+        "/upload", // deny access to upload page
     ],
 };
