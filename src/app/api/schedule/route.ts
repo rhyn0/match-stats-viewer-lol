59857e39cd9e637e4fbe64bbd6eee0d26d4ff377
Ryan Ozawa <ryan.ozawa21@gmail.com>
2025-01-29 23:27:49 -0800

    fix: math is hard for configs

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -2,7 +2,7 @@ import { listMatchesWithTeams } from "@/lib/crud/matchups";
 import db from "@/lib/drizzle-db";
 import { NextResponse } from "next/server";
 
-export const revalidate = 3600 * 2; // 2 hour in seconds
+export const revalidate = 7200; // 2 hour in seconds
 
 export async function GET(): Promise<NextResponse> {
     const result = await listMatchesWithTeams(db);
