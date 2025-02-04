6413e162d9c6c8a2cee61a8ac5d67a1ca24d8fb7
Ryan Ozawa <ryan.ozawa21@gmail.com>
2025-02-04 12:39:45 -0800

    Closes 31 Stop Champion Name typo (#32)
    * [upload] change: make champion combobox selector
    
    * change: spacing and sizing
    
    * change: champ name before check

---
+++ src/features/upload-form/components/champ-selector.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command";
+import { FormControl } from "@/components/ui/form";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/cn";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { allChampions } from "../constants";
+
+export interface ChampionFormComboboxProps {
+    value: string;
+    onSelect: (champ: string) => void;
+}
+
+function ChampionFormCombobox({ value, onSelect }: ChampionFormComboboxProps) {
+    return (
+        <Popover defaultOpen={false}>
+            <PopoverTrigger asChild>
+                <FormControl>
+                    <Button
+                        variant="outline"
+                        // biome-ignore lint/a11y/useSemanticElements:
+                        role="combobox"
+                        className="w-[200px] justify-between flex"
+                    >
+                        {value
+                            ? allChampions.find((champ) => champ === value)
+                            : "Select champion..."}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className="w-[160px] max-w-min p-0">
+                <Command>
+                    <CommandInput placeholder="Search framework..." />
+                    <CommandList>
+                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandGroup>
+                            {allChampions.map((champ) => (
+                                <CommandItem
+                                    key={champ}
+                                    value={champ}
+                                    onSelect={() => onSelect(champ)}
+                                    className="text-left"
+                                >
+                                    {champ}
+                                    <Check
+                                        className={cn(
+                                            "ml-2 mr-0 h-4 w-4",
+                                            champ === value
+                                                ? "opacity-100"
+                                                : "opacity-0",
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    );
+}
+
+export default ChampionFormCombobox;
